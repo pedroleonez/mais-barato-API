75f48cdddbdc24da4b4b0a43a8e357345f2ac758
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-14 16:29:23 -0300

    Create ProductModel.java

---
+++ src/main/java/pedroleonez/maisbarato/model/ProductModel.java
@@ -0,0 +1,44 @@
+package pedroleonez.maisbarato.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pedroleonez.maisbarato.model.enums.Unit;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private double price1;
+
+    private double size1;
+
+    private double price2;
+
+    private double size2;
+
+    @Enumerated(EnumType.STRING)
+    private Unit unit;
+
+    @ManyToMany(mappedBy = "products")
+    private Set<ShoppingListModel> shoppingLists = new LinkedHashSet<>();
+}
