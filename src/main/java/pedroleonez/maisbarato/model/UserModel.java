401d465581fe840d508e1858582485ec7f7e26b6
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-14 16:29:30 -0300

    Create UserModel.java

---
+++ src/main/java/pedroleonez/maisbarato/model/UserModel.java
@@ -0,0 +1,38 @@
+package pedroleonez.maisbarato.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ShoppingListModel> shoppingLists = new LinkedHashSet<>();
+}
