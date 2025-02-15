9613b25ca393650d07355f50b738aa09de61e909
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-15 15:21:30 -0300

    Create ShoppingListController.java

---
+++ src/main/java/pedroleonez/maisbarato/controllers/ShoppingListController.java
@@ -0,0 +1,16 @@
+package pedroleonez.maisbarato.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import pedroleonez.maisbarato.service.ShoppingListService;
+
+@RestController
+@RequestMapping("/shopping-lists")
+public class ShoppingListController {
+
+    private final ShoppingListService shoppingListService;
+
+    public ShoppingListController(ShoppingListService shoppingListService) {
+        this.shoppingListService = shoppingListService;
+    }
+}
+
