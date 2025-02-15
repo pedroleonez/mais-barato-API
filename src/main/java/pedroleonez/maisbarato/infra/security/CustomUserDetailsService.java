078773e824b1e771613f72782fc5b5f343a40536
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-15 15:21:48 -0300

    Create CustomUserDetailsService.java

---
+++ src/main/java/pedroleonez/maisbarato/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package pedroleonez.maisbarato.infra.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import pedroleonez.maisbarato.domain.model.UserModel;
+import pedroleonez.maisbarato.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
