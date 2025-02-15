e3215a7a389bbe7673709dbe8b6808583c07b679
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-15 15:21:27 -0300

    Create ProductController.java

---
+++ src/main/java/pedroleonez/maisbarato/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package pedroleonez.maisbarato.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import pedroleonez.maisbarato.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
+
