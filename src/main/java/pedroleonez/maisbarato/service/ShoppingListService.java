e19c9285fac572f8dd6df08f6133ba2be52af5e7
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-15 01:45:19 -0300

    Update ShoppingListService.java

--- src/main/java/pedroleonez/maisbarato/service/ShoppingListService.java
+++ src/main/java/pedroleonez/maisbarato/service/ShoppingListService.java
@@ -1,19 +1,9 @@
 package pedroleonez.maisbarato.service;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import pedroleonez.maisbarato.model.ProductModel;
-import pedroleonez.maisbarato.model.ShoppingListModel;
-import pedroleonez.maisbarato.model.UserModel;
 import pedroleonez.maisbarato.repository.ProductRepository;
 import pedroleonez.maisbarato.repository.ShoppingListRepository;
 import pedroleonez.maisbarato.repository.UserRepository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 
 @Service
 public class ShoppingListService {
