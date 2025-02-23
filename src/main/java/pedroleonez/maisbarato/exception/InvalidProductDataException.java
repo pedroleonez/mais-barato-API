ffe0831d95f2561e3cb29d5f9cadc3c31659bb02
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-23 16:05:34 -0300

    Create InvalidProductDataException.java

---
+++ src/main/java/pedroleonez/maisbarato/exception/InvalidProductDataException.java
@@ -0,0 +1,8 @@
+package pedroleonez.maisbarato.exception;
+
+public class InvalidProductDataException extends RuntimeException {
+
+    public InvalidProductDataException(String message) {
+        super(message);
+    }
+}
